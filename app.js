50793caa95ba8e78a2492b82e422ba2013f13566
Gavin W <gwilliams@search365.ai>
2021-02-01 21:50:08 +1100

    union types

--- app.js
+++ app.js
@@ -1,32 +1,14 @@
-// const person: {
-//   name: string;
-//   age: number;
-// } = {
-// const ADMIN = 0;
-// const READ_ONLY = 1;
-// const AUTHOR = 2;
-// applies a number based on position
-var Role;
-(function (Role) {
-    Role[Role["ADMIN"] = 0] = "ADMIN";
-    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
-    Role[Role["AUTHOR"] = 2] = "AUTHOR";
-})(Role || (Role = {}));
-;
-var person = {
-    name: 'Gavin',
-    age: 33,
-    hobbies: ['Sports', 'Cooking'],
-    role: Role.ADMIN
-};
-var favoriteActivities;
-favoriteActivities = ['Sports'];
-console.log(person.name);
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby.toUpperCase());
-    // console.log(hobby.map()); // !!! ERROR !!!
+function combine(input1, input2) {
+    var result;
+    if (typeof input1 === "number" && typeof input2 === "number") {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
 }
-if (person.role === Role.AUTHOR) {
-    console.log('Author');
-}
+var combinedAges = combine(30, 26);
+console.log(combinedAges);
+var combinedNames = combine("Chris", "Amy");
+console.log(combinedNames);
