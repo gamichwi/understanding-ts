79e17154ecc6edd01848c80943e88c67fc666b82
Gavin W <gwilliams@search365.ai>
2021-02-01 21:44:10 +1100

    objs-arrays-enums

--- app.js
+++ app.js
@@ -1,5 +1,32 @@
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+// applies a number based on position
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
+;
 var person = {
     name: 'Gavin',
-    age: 33
+    age: 33,
+    hobbies: ['Sports', 'Cooking'],
+    role: Role.ADMIN
 };
-console.log(person.nickname);
+var favoriteActivities;
+favoriteActivities = ['Sports'];
+console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+    // console.log(hobby.map()); // !!! ERROR !!!
+}
+if (person.role === Role.AUTHOR) {
+    console.log('Author');
+}
