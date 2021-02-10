040ce745f4217842c7a292c7c2f4637964b4d3d2
Gavin W <gwilliams@search365.ai>
2021-02-10 20:06:31 +1100

    first class with methods and private/public

--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,22 @@
 "use strict";
 class Department {
     constructor(n) {
+        this.employees = [];
         this.name = n;
+    }
+    describe() {
+        console.log("Department: " + this.name);
+    }
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+    printEmployeeInformation() {
+        console.log(this.employees.length, 'employee/s');
+        console.log(this.employees);
     }
 }
 const accounting = new Department("Accounting");
-console.log(accounting);
+accounting.addEmployee('Jim');
+accounting.addEmployee('Tom');
+accounting.printEmployeeInformation();
+accounting.describe();
--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,40 @@
 class Department {
   name: string;
+  private employees: string[] = [];
 
   constructor(n: string) {
     this.name = n;
   }
+
+  describe(this: Department) {
+    console.log("Department: " + this.name);
+  }
+
+  addEmployee(employee:string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length, 
+      'employee/s');
+    console.log(this.employees);
+  }
 }
+
 
 const accounting = new Department("Accounting");
 
-console.log(accounting);
+accounting.addEmployee('Jim');
+accounting.addEmployee('Tom');
+
+
+
+accounting.printEmployeeInformation();
+accounting.describe();
+
+
+// accounting.describe();
+
+// // const accountingCopy = { name: "DUMMY", describe: accounting.describe };
+
+// accountingCopy.describe();
